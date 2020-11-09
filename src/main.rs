4d0304519ce120f83e2780b2cdb9be0cec630368
Lulzmatt27 <lulmatt27@gmail.com>
2020-11-09 20:40:57 +0300

    add modules

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use server::Server;
+use http::Request;
+use http::Method;
+mod http;
+mod server;
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+    // println!("{:?}", Method);
+}
+// mod http {
+//     pub mod request {
+       
+//     }
+   
+//     pub mod method {
+       
+//     }
+// }
