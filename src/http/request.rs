9a683ba808e54ad101cf0a68cb84fc75d34ef657
lulz <lulmatt27@gmail.com>
2020-11-12 22:32:27 +0500

    add try_from for convert types

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,14 @@
 use super::method::Method;
-pub struct Request{
+use std::convert::TryFrom;
+pub struct Request {
       path: String,
       query_string: Option<String>,
-       method: Method,
+      method: Method,
+}
+
+impl TryFrom<&[u8]> for Request {
+      type Error = String;
+      fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+            unimplemented!()
+      }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,41 +1,44 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::io::Read;
 use std::net::TcpListener;
-pub struct Server{
-        addr: String,
+pub struct Server {
+  addr: String,
 }
-impl Server{
-        pub fn new(addr: String) -> Self{
-            Self{
-                addr
+impl Server {
+  pub fn new(addr: String) -> Self {
+    Self { addr }
+  }
+
+  pub fn run(self) {
+    println!("running on {}", self.addr);
+    let listener = TcpListener::bind(&self.addr).unwrap();
+
+    loop {
+      match listener.accept() {
+        Ok((mut stream, addr)) => {
+          let mut buffer: [u8; 1024] = [0; 1024];
+          match stream.read(&mut buffer) {
+            Ok(_) => {
+              println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+              match Request::try_from(&buffer[..]) {
+                Ok(request) => {}
+                Err(e) => println!("Failed to parse a request: {}", e),
+              }
             }
+            Err(e) => println!("Failed to read from connection: {}", e),
+          }
+          // println!("{:?}", buffer[0]);
         }
-
-        pub fn run(self){
-            println!("running on {}", self.addr);
-            let listener = TcpListener::bind(&self.addr).unwrap();
-            
-            loop{
-              match listener.accept(){
-                Ok((mut stream, addr)) =>{
-                  let mut buffer: [u8; 1024] = [0; 1024]; 
-                  match stream.read(&mut buffer){
-                    Ok(_)=>{
-                      println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                    },
-                    Err(e) => println!("Failed to read from connection: {}", e),
-                  }
-                  // println!("{:?}", buffer[0]);
-
-                },
-                Err(e) => println!("Failed to establish a connection: {}", e),
-              }
-              let res = listener.accept();
-
-              if res.is_err(){
-                continue;
-              }
-              let (stream, addr) = res.unwrap();
-
+        Err(e) => println!("Failed to establish a connection: {}", e),
       }
+      let res = listener.accept();
+
+      if res.is_err() {
+        continue;
+      }
+      let (stream, addr) = res.unwrap();
     }
+  }
 }
