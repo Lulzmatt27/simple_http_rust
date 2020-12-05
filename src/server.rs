88535de0d43e114b65fab4b2134ee1777676f8a5
Lulzmatt27 <lulmatt27@gmail.com>
2020-12-05 20:32:17 +0300

    add response and handle tcp stream

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
-use crate::http::Request;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
-use std::convert::TryInto;
-use std::io::Read;
+// use std::convert::TryInto;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 pub struct Server {
   addr: String,
@@ -22,9 +22,19 @@ impl Server {
           match stream.read(&mut buffer) {
             Ok(_) => {
               println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-              match Request::try_from(&buffer[..]) {
-                Ok(request) => {}
-                Err(e) => println!("Failed to parse a request: {}", e),
+              let response = match Request::try_from(&buffer[..]) {
+                Ok(request) => {
+                  dbg!(request);
+                  Response::new(StatusCode::Ok, Some("<h1>hello</h1>".to_string()))
+                  // write!(stream, "{}", response);
+                }
+                Err(e) => {
+                  println!("Failed to parse a request: {}", e);
+                  Response::new(StatusCode::BadRequest, None)
+                },
+              };
+              if let Err(e) = response.send(&mut stream){
+                println!("Failed to send response: {}", e);
               }
             }
             Err(e) => println!("Failed to read from connection: {}", e),
